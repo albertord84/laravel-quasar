4b8ff433190d1c240e18144a905a51e9815bc115
Your Name <you@example.com>
2020-02-08 01:01:05 -0300

    Alberto Initial Commit All Working

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+		public function index(Request $request)
+		{
+      return response()->json([
+          'copyright' => 'Manfred047',
+          'author' => 'Emanuel Chablé Concepción',
+          'email' => 'manfred@manfred047.com',
+          'website' => 'https://manfred047.com',
+          'github' => 'https://github.com/Manfred047'
+      ]);
+		}
+}
