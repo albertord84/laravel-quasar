59c94354759e5bdc1fe2a305a1418586acb2f50a
josergm86 <josergm86@gmail.com>
2020-02-26 08:34:43 -0300

    a

--- database/seeds/UsersRolesTableSeeder.php
+++ database/seeds/UsersRolesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersRolesTableSeeder extends Seeder
 {
@@ -11,6 +12,32 @@ class UsersRolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      $this->command->info('Truncate Users Table...');
+      // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+      // DB::table('users_managers')->truncate();
+      // DB::table('users_attendants')->truncate();
+      DB::table('users')->truncate();
+      // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+      $this->command->info('Creating Users:');
+
+      $this->createUserRoles();
     }
+
+    public function createUserRoles(){
+      // User::create([
+      //     'id' => 1,
+      //     'company_id' => 1,
+      //     'cost_center_id' => 1,
+      //     'address_id' => 1,
+      //     'username' => 'Sinei',
+      //     'email' => 'sidnei.bertozzi@gmail.com',
+      //     'password' => bcrypt('sidnei.bertozzi123'),
+      //     'role_id' => 1,
+      //     'status_id' => 1,
+      // ]);
+      // $this->command->info('Admin created: [user: sidnei.bertozzi]');
+    }
+
+
 }
