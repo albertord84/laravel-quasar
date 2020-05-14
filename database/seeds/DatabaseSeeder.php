06e836821c367636dffadfff7f21ba595fc17d7c
josergm86 <josergm86@gmail.com>
2020-05-14 12:35:58 -0300

    a

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,7 +27,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CostsCentersTableSeeder::class);
         $this->call(BasesTableSeeder::class);
 
-
         $this->call(UsersTableSeeder::class);
         $this->call(UsersBasesTableSeeder::class);
 
