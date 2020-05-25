81213fd17cc622521cd510551d5579fc4a899f5e
Your Name <you@example.com>
2020-05-25 19:33:24 -0300

    Auto message

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
 		public function index(Request $request)
 		{
+      dd(Auth::guard('web'));
       return view('app');
 		}
 }
